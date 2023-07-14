9fc07f9e6461e943f069843d6f7957f96324953c
jjjjltd <johnjoyce90@hotmail.co.uk>
2023-07-14 11:01:59 +0100

    Remove secrets confi

--- defs.py
+++
@@ -1,7 +0,0 @@
-API_KEY = "bcc46bed475efe2112ef16229c1d5ca5-aeb23ae10442e0b18e6eed31bbdd36a9"
-ACCOUNT_ID = "101-004-26248413-001"
-OANDA_URL = "https://api-fxpractice.oanda.com/v3"
-
-SECURE_HEADER = {
-    'Authorization': f'Bearer {API_KEY}'
-}
