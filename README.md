5b6ea54229c4f4b9b7edbdb67769a307d1879a03
jjjjltd <johnjoyce90@hotmail.co.uk>
2023-07-17 15:11:26 +0100

    Updated instrument class, prep for backtesting.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # OANDA-Bluefever
-Following fantastic Youtube videos to build Algorithmic Trading application (eventually)
+Following fantastic Youtube videos to build Algorithmic Trading application (eventually)...
+
+Latest progress:  https://www.youtube.com/watch?v=nMOXozRTVXA&list=PLZ1QII7yudbecO6a-zAI6cuGP1LLnmW8e&index=22
+candle_plot_ma1.ipynb
+
--- instrument.py
+++ instrument.py
@@ -22,7 +22,23 @@ class Instrument():
         df = cls.get_instruments_df()
         return [Instrument(x) for x in df.to_dict(orient='records')]
 
+    @classmethod
+    def get_instruments_dict(cls):
+        i_list = cls.get_instruments_list()
+        i_keys = [x.ins_name for x in i_list]
+        return {k:v for (k, v) in zip(i_keys, i_list)}
 
+    @classmethod
+    def get_instrument_by_name(cls, pairname):
+        d = cls.get_instruments_dict()
+
+        if pairname in d:
+            return d[pairname]
+        else:
+            return None
 
 if __name__ == "__main__":
     print(Instrument.get_instruments_list())
+    for k, v in Instrument.get_instruments_dict().items():
+        print(k, v)
+    print(Instrument.get_instrument_by_name("AUD_SGD"))
