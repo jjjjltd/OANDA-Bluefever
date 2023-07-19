12ed6cdb53612ce9c0e8aa0a39fea429f9c58192
jjjjltd <johnjoyce90@hotmail.co.uk>
2023-07-19 09:44:37 +0100

    Capture basic data for multi ma tests.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # OANDA-Bluefever
 Following fantastic Youtube videos to build Algorithmic Trading application (eventually)...
 
-Latest progress:  https://www.youtube.com/watch?v=kIgVLFpRmqc
+Latest progress:  https://www.youtube.com/watch?v=MvHqhRE9B3E
 ma_sim.py
 
--- ma_sim.py
+++ ma_sim.py
@@ -56,28 +56,49 @@ def evaluate_pair(i_pair, mashort, malong, price_data):
 def process_results(results):    
     results_list = [r.result_obj() for r in results]
     final_df = pd.DataFrame.from_dict(results_list)
+    final_df.to_csv("./Data/ma_test_results.csv")
 
-    print(final_df.info())
-    print(final_df.head())
+    print(final_df.shape, final_df.num_trades.sum())
+
+def get_existing_pairs(pair_str):
+    existing_pairs = instrument.Instrument.get_instruments_dict().keys()
+    pairs = pair_str.split(",")
+
+    test_list = []
+
+    # Loop through pairs list twice (each currency within each currency)
+    for p1 in pairs:
+        for p2 in pairs:
+            p = f"{p1}_{p2}"
+            if p in existing_pairs:
+                test_list.append(p)
+
+    return test_list
 
 def run():
 
-    pairname = "GBP_JPY"
+    pair_str = "GBP,JPY,USD,CAD,EUR,CHF,NZD"
     granularity = "H1"
-    ma_short = [8, 16, 32, 64]
-    ma_long = [32, 64, 96, 128, 256]
-    i_pair = instrument.Instrument.get_instruments_dict()[pairname]
+    ma_short = [4, 8, 16, 24, 32, 64]
+    ma_long = [8, 16, 32, 64, 96, 128, 256]
 
-    price_data = get_price_data(pairname, granularity)
-    price_data = process_data(ma_short, ma_long, price_data)
+    test_pairs = get_existing_pairs(pair_str)
+
+    
 
     results = []
+    for pairname in test_pairs:
+        print("running pair...", pairname)
+        i_pair = instrument.Instrument.get_instruments_dict()[pairname]
 
-    for _malong in ma_long:
-        for _mashort in ma_short:
-            if _mashort >= _malong:
-                continue
-            results.append(evaluate_pair(i_pair, _mashort, _malong, price_data.copy()))
+        price_data = get_price_data(pairname, granularity)
+        price_data = process_data(ma_short, ma_long, price_data)
+
+        for _malong in ma_long:
+            for _mashort in ma_short:
+                if _mashort >= _malong:
+                    continue
+                results.append(evaluate_pair(i_pair, _mashort, _malong, price_data.copy()))
             
     process_results(results)
 
