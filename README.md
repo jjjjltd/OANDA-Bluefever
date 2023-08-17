5e5f99218ac0707bc8080ba75d3a76d475fb6a23
jjjjltd <johnjoyce90@hotmail.co.uk>
2023-08-17 09:44:58 +0100

    Excellent lesson in CSS and Roboto fonts.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # OANDA-Bluefever
 Following fantastic Youtube videos to build Algorithmic Trading application (eventually)...
 
-Latest progress: https://www.youtube.com/watch?v=SSsbnhd4NJc&list=PLZ1QII7yudbecO6a-zAI6cuGP1LLnmW8e&index=60
+Latest progress: https://www.youtube.com/watch?v=6XFz2inPnhQ&list=PLZ1QII7yudbecO6a-zAI6cuGP1LLnmW8e&index=61
 WebDash directory
 
 # Back Story
--- WebDash/static/app.js
+++ WebDash/static/app.js
@@ -5,15 +5,10 @@ var app = new Vue({
         kpiData: undefined
     },
     methods: {
-        // Note:  function needs to be declared async, otherwise the await(s) on the const(s) won't work.
         loadData: async function() {
-            // console.log("Load Data Was Clicked!");
             const response = await fetch('data.json');
             const data = await response.json();
-            // console.log(response);
-            // console.log(data)
-            this.kpiData = data;
-            
+            this.kpiData = data;            
         }
     }
 })
