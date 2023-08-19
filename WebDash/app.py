d06c3351ad829d1a6895084257813be774fa1102
jjjjltd <johnjoyce90@hotmail.co.uk>
2023-08-19 12:44:05 +0100

    Import/use schedule to automate.  TODO: Get new flask route to respond.

--- WebDash/app.py
+++ WebDash/app.py
@@ -1,10 +1,18 @@
 from flask import Flask, jsonify
 from whitenoise import WhiteNoise
+import json
 
 app = Flask(__name__)
 app.wsgi_app = WhiteNoise(
     app.wsgi_app, root="static/", index_file=True, autorefresh=True
 )
+
+@app.route("/kpi_data")
+def get_kpi_data():
+    # TODO:  Add robustness to this.
+    with open('data.json', 'r') as f:
+        data = json.loads(f.read())
+        return data              
 
 if __name__ == "__main__":
     app.run()
